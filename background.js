c01ad4c117bc2a2a5fab09e16097dba1cb89389c
IamCathal <cathalsocallaghan@gmail.com>
2020-01-04 23:10:05 +0000

    Cleaned and simplifieed code
    Code is more straight forward and laid out in a more clear manner.

--- background.js
+++
@@ -1,9 +0,0 @@
-//get all contents of chrome storage
-chrome.storage.sync.get(null, function(obj) {
-    // console.log(JSON.stringify(obj));
-    console.log(obj);
-});
-
-// chrome.storage.sync.clear(function() {
-//     console.log("Cleared!");
-// })
